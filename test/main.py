1098ba4a1c1444028450b704bc9adee998ed0ede
Misha91908 <misha91908@gmail.com>
2017-10-28 15:36:55 +0300

    added a new version code

--- test/main.py
+++ test/main.py
@@ -1,27 +1,103 @@
-import threading
 import time
-from PyQt5 import QtWidgets
 import sys
+from PyQt5 import QtCore, QtWidgets
 
-def start_gui():
+class countingWorker(QtCore.QObject):
+    # explicit signal sent in the startCounting function
+    set_box_signal = QtCore.pyqtSignal(int, str)
+
+    # explicit slot that takes input from the start_counting_signal
+    @QtCore.pyqtSlot(int, str)
+    def startCounting(self, boxNumber,):
+        value = int()
+        while True:
+            value = 1 + value
+            self.set_box_signal.emit(boxNumber, str(value))
+            time.sleep(1)
+
+class firstGui(QtWidgets.QDialog):
+    # explicit signals from the start_counting_emitter function
+    n = 2  # make this many counters
+    start_counting_signal = QtCore.pyqtSignal(int, str)
+
+    def __init__(self):
+        super(firstGui, self).__init__()
+
+        self.workers = []
+        self.threads = []
+        for j in range(self.n):
+            self.workers.append(countingWorker())
+            self.threads.append(QtCore.QThread())
+            self.workers[j].moveToThread(self.threads[j])
+        self.init_ui()
+        self.resize(1000, 500)
+
+    def init_ui(self):
+
+        self.boxes = []
+        self.starts = []
+        self.stops = []
+        for j in range(self.n):
+            # Creating and naming the boxes
+            self.boxes.append(QtWidgets.QTextEdit(self))
+            self.boxes[j].setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+            self.boxes[j].setObjectName('box'+str(j))
+            # creating the start buttons
+            self.starts.append(QtWidgets.QPushButton('Start', self))
+            self.starts[j].setObjectName('start' + str(j))
+            self.stops.append(QtWidgets.QPushButton('Stop', self))
+            self.stops[j].setObjectName('stop' + str(j))
+
+        # Creating a Horizontal Layout to add all the widgets
+        self.hboxLayout = QtWidgets.QHBoxLayout(self)
+
+        # Adding the widgets
+        for j in range(self.n):
+            self.hboxLayout.addWidget(self.boxes[j])
+            self.hboxLayout.addWidget(self.starts[j])
+            self.hboxLayout.addWidget(self.stops[j])
+
+        # Setting the hBoxLayout as the main layout
+        self.setLayout(self.hboxLayout)
+        self.setWindowTitle('gui_4b')
+
+        # making all of the connections
+        for j in range(self.n):
+            self.starts[j].clicked.connect(self.start_counting_emitter)
+            self.stops[j].clicked.connect(self.stop_counting)
+            self.workers[j].set_box_signal.connect(self.set_box_text)
+
+        # show the GUI
+        self.show()
+
+    # this is an implicit slot for the startbuttons to connect to
+    def start_counting_emitter(self):
+        start_button_name = self.sender().objectName()
+        start_number = int(start_button_name[5:])
+        self.start_counting_signal.connect(self.workers[start_number].startCounting)
+        if not self.threads[start_number].isRunning():
+           self.threads[start_number].start()
+           print('thread '+str(start_number)+' started')
+           self.start_counting_signal.emit(start_number, self.boxes[start_number].toPlainText())
+        self.start_counting_signal.disconnect(self.workers[start_number].startCounting)
+
+    # implicit slot of the stop buttons
+    def stop_counting(self):
+        stop_button_name = self.sender().objectName()
+        stop_number = int(stop_button_name[4:])
+        if self.threads[stop_number].isRunning():
+            self.threads[stop_number].terminate()
+            print('thread '+str(stop_number)+' terminated')
+
+    # explicit slot that takes input from the countingWorker and sets the text of the provided box
+    @QtCore.pyqtSlot(int, str)
+    def set_box_text(self, boxNumber):
+        self.boxes[boxNumber].insertPlainText(str(time.time()) + '\n')
+
+def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QWidget()
-    window.resize(800, 500)
-    txt = QtWidgets.QTextEdit(window)
-    txt.resize(800, 495)
-    send_txt(txt)
-    window.show()
+    a = firstGui()
     sys.exit(app.exec_())
 
-def send_txt(txt):
-    txt.setText("Hello World\n")
-    time.sleep(1)
-
-thread1 = threading.Thread(target=start_gui)
-thread2 = threading.Thread(target=send_txt)
-
-thread1.start()
-thread2.start()
-
-thread1.join()
-thread2.join()
+if __name__ == '__main__':
+    main()
